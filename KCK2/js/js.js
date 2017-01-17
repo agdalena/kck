250829a82f6b1fbd5d0c095c60da6c080e8a64c1
agdalena <chica.bizcochito@gmail.com>
2017-01-17 21:16:08 +0100

    Initial

---
+++ KCK2/js/js.js
@@ -0,0 +1,63 @@
+﻿// Accordion
+function myAccFunc1() {
+    var x = document.getElementById("book");
+        if (x.className.indexOf("w3-show") == -1) {
+            x.className += " w3-show";
+        } else {
+            x.className = x.className.replace(" w3-show", "");
+        }
+}
+
+function myAccFunc2() {
+    var x = document.getElementById("ebook");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
+
+function myAccFunc3() {
+    var x = document.getElementById("film");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
+
+function myAccFunc4() {
+    var x = document.getElementById("music");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
+
+function myAccFunc5() {
+    var x = document.getElementById("news");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
+
+
+// Click on the "Jeans" link on page load to open the accordion for demo purposes
+document.getElementById("myBtn1").click();
+document.getElementById("myBtn2").click();
+
+
+
+// Script to open and close sidenav
+function w3_open() {
+    document.getElementById("mySidenav").style.display = "block";
+    document.getElementById("myOverlay").style.display = "block";
+}
+
+function w3_close() {
+    document.getElementById("mySidenav").style.display = "none";
+    document.getElementById("myOverlay").style.display = "none";
+}
